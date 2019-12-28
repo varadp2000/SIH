dea6d0c1d2c1e554621bc32c5b18c414668dad12
hritikkothari <kotharihrithik1@gmail.com>
2019-12-29 00:44:19 +0530

    Organized the navbar : Site still not responsive.

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+
+        <script
+        src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"
+        crossorigin="anonymous"></script>
+        <script type="text/javascript" src="public/js/index.js"></script>
+
+        <script>
+            // Get the current year for the copyright
+            $('#year').text(new Date().getFullYear());
+          </script>
+    </body>
+
+</html>
--- login.php
+++ login.php
@@ -1,25 +1,13 @@
-<?php 
+<?php require_once('includes/header.php'); ?>
 
-require_once 'navbar.php';
-
-?>
-
-
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body style="background-image:url('loginbg.jpg');background-repeat:no-repeat;background-attachment: fixed;background-size: cover;">
-    <br><br><br><br>
-    <div class="container" style="padding:40px 250px">
-    <div class="card" style="opacity:0.9;height:70rem 100px;border-radius:2rem;background-color:black;width:40rem">
-    <br>
+    <div class="container">
+        <div class="card" style="opacity:0.9;height:70rem 100px;border-radius:2rem;background-color:black;width:40rem">
         <div style="width:100%; text-align:center;">
             <img class="card-img-top" src="avtar2.jpg" style="width:15%;height:15%;border-radius:100%;"></img>
             <h3 class="card-title " style="color:white">Sign in</h3>
         </div>
         <div class="card-body">
-            
+
             <div class="card-text">
             <form method="post" class="">
             <input type="email" placeholder="Email" name="email" class="form-control btn-outline-danger" style="background-color:white"><br>
@@ -37,20 +25,18 @@ require_once 'navbar.php';
         </div>
     </div>
     </div>
-    
-    <br><Br>
 
-</body>
-</html>
+    <script language="JavaScript">
 
-<Script language="JavaScript">
-function showPass() {
-  var x = document.getElementById("pass");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-}
+        function showPass() {
+            var x = document.getElementById("pass");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        }
 
-</script>
+    </script>
+
+<?php require_once('includes/footer.php'); ?>
