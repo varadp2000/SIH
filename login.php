da2cb8389b9a7dc0cf303ccf16d0842e9ea24461
Varad Patil <varadpatil@gmail.com>
2019-12-29 01:42:39 +0530

    Fixed Navbar and login page

--- login.php
+++ login.php
@@ -1,58 +1,40 @@
-<?php 
-
-require_once 'navbar.php';
-
-?>
-
-
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body style="background-image:url(loginbg1.jpg);background-repeat:no-repeat;background-attachment: fixed;background-size: cover;">
-    <br><br><br><br>
-    <div class="container" style="padding:40px 250px">
-    <div class="card" style="opacity:0.9;height:70rem 100px;border-radius:2rem;background-color:grey;width:40rem">
-    <br>
-        <div style=" text-align:center;">
-            <img class="card-img-top" src="avtar2.jpg" style="width:20%;height:20%;border-radius:100%;"></img>
+<?php require_once('includes/header.php'); ?>
+    <div class="container" >
+        <div class="card" style="margin-top:100px;padding:10% 10%;opacity:0.9;height:70rem 100px;border-radius:2rem;background-color:black;">
+        <div style="background-color:black;">
+        <div style="width:100%; text-align:center;">
+            <img src="public/images/avtar2.jpg" style="width:170px;height:170px;border-radius:100%;"></img>
             <h3 class="card-title " style="color:white">Sign in</h3>
         </div>
-        <div class="card-body">
-            
-            <div class="card-text">
+        <div >
+
+            <div>
             <form method="post" class="">
-            <label style="color:white">Email address</label>
-            <input type="email" placeholder="Email" name="email" class="form-control btn-outline-primary" style="background-color:white"><br>
-            <label style="color:white">Password</label>
-            <input type="password" placeholder="Password" ID="pass" name="pass" class="form-control btn-outline-primary" style="background-color:white"><br>
-            <input type="submit" class="btn btn-outline-light my-2 my-sm-0" >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input type="email" placeholder="Email" name="email" class="form-control" style="background-color:white"><br>
+            <input type="password" placeholder="Password" ID="pass" name="pass" class="form-control" style="background-color:white"><br>
+            <input type="submit" class="btn btn-outline-danger my-2 my-sm-0">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <u class="custom-switch btn" >
                 <input type="checkbox" class="custom-control-input" onclick="showPass()" id="customSwitches" >
                 <label class="custom-control-label" style="color:white;" for="customSwitches">Show Password</label>
             </u><br><br>
-            <a href="#" style="color:white">Forgot Password?</a><br>
-            <a href="#" style="color:white">Don't have an account??</a>
+            <a href="#" class="form-group" style="color:white">Forgot Password?</a><br>
+            <a href="#" class="form-group" style="color:white">Don't have an account??</a>
         </div>
         </form>
-        </div>
-        </div>
     </div>
     </div>
-    
-    <br><Br>
 
-</body>
-</html>
+    <script language="JavaScript">
 
-<Script language="JavaScript">
-function showPass() {
-  var x = document.getElementById("pass");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-}
+        function showPass() {
+            var x = document.getElementById("pass");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        }
 
-</script>
+    </script>
+
+<?php require_once('includes/footer.php'); ?>
